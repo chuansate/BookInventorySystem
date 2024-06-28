70efeca110d753229de60e7817bac1c2630dbbd6
chuansate <lowsimchuan@gmail.com>
2024-06-28 22:30:26 +0800

    Allowed user to save a book

--- app.js
+++ app.js
@@ -2,17 +2,23 @@ let books_frm_database = [
     {
         title: "Intro to C++",
         author: "James",
-        price: 100
+        price: 100,
+        img: "assets/1.jpg",
+        isFav: true,
     },
     {
         title: "Advanced Python",
         author: "Ben",
-        price: 250
+        price: 250,
+        img: "assets/2.jpg",
+        isFav: false,
     },
     {
         title: "Calculus",
         author: "Newton",
-        price: 125
+        price: 125,
+        img: "assets/3.jpg",
+        isFav: true,
     }
 ]
 
@@ -54,12 +60,16 @@ const app = Vue.createApp({
             if (index_to_delete > -1) { //if found
                 this.books.splice(index_to_delete, 1);
             } else {
-                console.log("The book '" + title + "' not found!")
+                console.log("The book '" + title + "' not found to be deleted!")
             }
         },
 
         toggleShowBooks() {
             this.showBooks = !this.showBooks
+        },
+
+        toggleSaveBook(book) {
+            book.isFav = !book.isFav;
         },
 
         mouseoverEdit(event) {
